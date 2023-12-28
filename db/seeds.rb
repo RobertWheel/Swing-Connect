b1d951cb10ff43aed5d18c3810252e386af200c4
riwheel <risaac4jc@icloud.com>
2023-12-28 00:58:21 -0800

    added grid css to display teams and players cleaner

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+=begin
 
 User.create(
   :email => "rob@swingsync.local",
@@ -103,7 +103,7 @@ Player.create(
 )
 
 
-=begin
+
 
 
 Schedule.create(
