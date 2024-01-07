b141577fcedf0f18d4d1280a6821ede51fd0c7c7
riwheel <risaac4jc@icloud.com>
2024-01-07 01:20:57 -0800

    re added the test server configs and the devise authentication

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,8 +67,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  #config.action_mailer.default_url_options = { host: "SwingSync.local", port: 3000 }
+  config.action_mailer.default_url_options = { host: "SwingSync.local", port: 3000 }
   config.action_mailer.delivery_method = :letter_opener_web                                       # Annotate rendered view with file names.
                                                                                                    # config.action_view.annotate_rendered_view_with_filenames = true
- # config.hosts << "swingsync.local"  
+  config.hosts << "swingsync.local"  
 end
